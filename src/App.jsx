0bfe01b259a45a560fd46da0ee1db071ab4273c9
blac-coffee <soumilgupta.812@gmail.com>
2026-02-03 21:51:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AuthInputs from "./components/AuthInputs.jsx";
+import Header from "./components/Header.jsx";
+import "./index-old.css";
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
+  );
+}
